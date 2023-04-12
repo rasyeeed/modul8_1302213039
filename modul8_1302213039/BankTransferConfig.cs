b90626a4b76a0b6200506b281d5f0502feabcf96
rasyeeed <muhammadnurrasyid@gmail.com>
2023-04-12 16:36:21 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302213039/BankTransferConfig.cs
@@ -0,0 +1,62 @@
+﻿using modul8_1302213039;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_1302213039
+{
+    public class BankTransferConfig
+    {
+        public BankTransfer config;
+
+        private const string filePath = "C:\\Telkom\\Semester 4\\KPL\\modul8_1302213039\\";
+        private const string fileLocation = filePath + "bank_transfer_config.json";
+
+        public BankTransferConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+
+        private BankTransfer ReadConfigFile()
+        {
+            string hasilBaca = File.ReadAllText(fileLocation);
+            config = JsonSerializer.Deserialize<BankTransfer>(hasilBaca);
+            return config;
+        }
+
+        private void SetDefault()
+        {
+            string lang = "id";
+            Transfer transfer = new Transfer(1000000, 6500, 19500);
+            List<string> methods = new List<string>();
+            methods.Add("RTO (real-time)");
+            methods.Add("SKN");
+            methods.Add("RTGS");
+            methods.Add("BI FAST");
+            Confirmation confirmation = new Confirmation("yes", "ya");
+            config = new BankTransfer(lang, transfer, methods, confirmation);
+        }
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+
+            string textTulis = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(fileLocation, textTulis);
+        }
+    }
+}
--- modul8_1302213039/Program.cs
+++ modul8_1302213039/Program.cs
@@ -1,2 +1,108 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul8_1302213039;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BankTransferConfig bankTransferRegy = new BankTransferConfig();
+        string language = bankTransferRegy.config.lang;
+
+        if (language == "id" || language == "en")
+        {
+            if (language == "en")
+            {
+                Console.Write("Please insert the amount of money to transfer: ");
+            }
+            else if (language == "id")
+            {
+                Console.Write("Masukkan jumlah uang yang akan di-transfer: ");
+            }
+
+
+            int jumlah = Convert.ToInt32(Console.ReadLine());
+            int biaya;
+            if (jumlah <= bankTransferRegy.config.transfer.threshold)
+            {
+                biaya = bankTransferRegy.config.transfer.low_fee;
+            }
+            else
+            {
+                biaya = bankTransferRegy.config.transfer.high_fee;
+            }
+
+
+            int total = jumlah + biaya;
+
+
+            if (language == "en")
+            {
+                Console.WriteLine("Transfer fee = " + biaya);
+                Console.WriteLine("Total amount = " + total);
+            }
+            else if (language == "id")
+            {
+                Console.WriteLine("Biaya transfer = " + biaya);
+                Console.WriteLine("Total biaya = " + total);
+            }
+
+
+            if (language == "en")
+            {
+                Console.WriteLine("Select tranfer method:");
+            }
+            else if (language == "id")
+            {
+                Console.WriteLine("Pilih metode transfer:");
+            }
+
+
+            for (int i = 0; i < bankTransferRegy.config.methods.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + bankTransferRegy.config.methods[i]);
+            }
+            Console.WriteLine("Ketik: ");
+            string metode = Console.ReadLine();
+
+
+            if (language == "en")
+            {
+                Console.WriteLine("Please type " + bankTransferRegy.config.confirmation.en +
+                    " to confirm the transaction: ");
+            }
+            else if (language == "id")
+            {
+                Console.WriteLine("Ketik " + bankTransferRegy.config.confirmation.id +
+                    " untuk mengkonfirmasi transaksi: ");
+            }
+            string konfirmasi = Console.ReadLine();
+
+
+            if (language == "en")
+            {
+                if (konfirmasi == bankTransferRegy.config.confirmation.en)
+                {
+                    Console.WriteLine("The transfer is completed");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer is cancelled");
+                }
+            }
+            else if (language == "id")
+            {
+                if (konfirmasi == bankTransferRegy.config.confirmation.id)
+                {
+                    Console.WriteLine("Proses transfer berhasil");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer dibatalkan");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Unknown language");
+        }
+    }
+}
